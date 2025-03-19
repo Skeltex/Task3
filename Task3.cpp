46e30777302148f8f2344981a807625bf7b2d433
Dmitry <dmtr.afanasyev@gmail.com>
2025-03-19 22:32:40 +0400

    Add Fibonacci function

--- Task3.cpp
+++ Task3.cpp
@@ -1,7 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
 
+int fib(int n) {
+    if (n <= 2)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
     cout << "Hello World!\n";
+    int n;
+    cin >> n;
+    cout << fib(n) << '\n';
     return 0;
 }
